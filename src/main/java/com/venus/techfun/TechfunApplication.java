9a04cd4195570184c0227148f8a24c3aaf0ecf53
Bolun Ma <mabl02@rd.netease.com>
2022-12-07 13:10:04 +0800

    Initial commit

---
+++ src/main/java/com/venus/techfun/TechfunApplication.java
@@ -0,0 +1,13 @@
+package com.venus.techfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechfunApplication.class, args);
+	}
+
+}
